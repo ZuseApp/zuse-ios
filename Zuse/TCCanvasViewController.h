d812ff8f0a291c9e924216c7da3daf5a3a83dff6
Michael Hogenson <mah.netuse@live.com>
2013-10-01 17:20:31 -0600

    Project JSON skeleton, need interpreter to start working now.

--- Zuse/TCCanvasViewController.h
+++ Zuse/TCCanvasViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TCCanvasViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface TCCanvasViewController : UIViewController
 
 @end
--- Zuse/TCCanvasViewController.m
+++ Zuse/TCCanvasViewController.m
@@ -11,15 +11,14 @@
 #import "TCSprite.h"
 #import "TCSpriteView.h"
 #import "TCSpriteManager.h"
+#import "INInterpreter.h"
 
 @interface TCCanvasViewController ()
 
 @property (nonatomic, strong) TCSpriteManager *spriteManager;
 @property (nonatomic, strong) NSArray *templateSprites;
 @property (nonatomic, strong) NSArray *canvasSprites;
-@property (weak, nonatomic) IBOutlet TCSpriteView *paddleOne;
-@property (weak, nonatomic) IBOutlet TCSpriteView *paddleTwo;
-@property (weak, nonatomic) IBOutlet TCSpriteView *ball;
+@property (strong, nonatomic) INInterpreter *interpreter;
 
 @end
 
@@ -28,11 +27,56 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _spriteManager = [TCSpriteManager sharedManager];
+    NSString *jsonPath = [[NSBundle mainBundle] pathForResource:@"TestProject" ofType:@"json"];
+    NSData *jsonData = [NSData dataWithContentsOfFile:jsonPath];
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:nil];
+    // NSLog(@"%@", json);
+    NSDictionary *jsonObject = [json[@"objects"] firstObject];
+    NSDictionary *variables = jsonObject[@"variables"];
+    CGRect frame = CGRectZero;
     
-    _ball.touchesMoved = ^(UITouch *touch) {
-        
-    };
+    frame.origin.x = [variables[@"x"] floatValue];
+    frame.origin.y = [variables[@"y"] floatValue];
+    frame.size.width = [variables[@"width"] floatValue];
+    frame.size.height = [variables[@"height"] floatValue];
+    
+    UIView *view = [[UIView alloc] initWithFrame:frame];
+    view.backgroundColor = [UIColor blackColor];
+    
+    [self.view addSubview:view];
+    
+    _interpreter = [[INInterpreter alloc] init];
+    
+    // [_interpreter loadObjects:json[@"objects"]];
+    
+    [_interpreter loadMethod:@{
+        @"name": @"ask",
+        @"block":^id(NSArray *args) {
+
+            return @8;
+        }
+    }];
+    
+    [_interpreter loadMethod:@{
+        @"name": @"display",
+        @"block":^id(NSArray *args) {
+            UIAlertView *alertView = [[UIAlertView alloc] init];
+            [alertView addButtonWithTitle:@"OK"];
+            [alertView setTitle:args[0]];
+            [alertView show];
+            return nil;
+        }
+    }];
+    
+    [_interpreter loadMethod:@{
+        @"name": @"random_number",
+        @"block":^id(NSArray *args) {
+            NSInteger min = [args[0] integerValue];
+            NSInteger max = [args[1] integerValue];
+            NSUInteger rand_num = arc4random_uniform(max) + min;
+            return @(rand_num);
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning
