3c28a60f63f7fdb1f8b41e427ba74b8cc4b7a5fb
Michael Hogenson <mah.netuse@live.com>
2013-10-01 20:31:46 -0600

    Added panning gestures to hide and show a table.

---
+++ Zuse/ZSCanvasViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZSCanvasViewController.h
+//  Zuse
+//
+//  Created by Michael Hogenson on 10/1/13.
+//  Copyright (c) 2013 Michael Hogenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZSCanvasViewController : UIViewController <UIGestureRecognizerDelegate>
+
+@end
---
+++ Zuse/ZSCanvasViewController.m
@@ -0,0 +1,82 @@
+//
+//  ZSCanvasViewController.m
+//  Zuse
+//
+//  Created by Michael Hogenson on 10/1/13.
+//  Copyright (c) 2013 Michael Hogenson. All rights reserved.
+//
+
+#import "ZSCanvasViewController.h"
+
+@interface ZSCanvasViewController ()
+    @property (weak, nonatomic) IBOutlet UITableView *spriteTable;
+    @property (strong, nonatomic) UIScreenEdgePanGestureRecognizer *screenRecognizer;
+
+@end
+
+@implementation ZSCanvasViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _screenRecognizer = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(canvasPanned:)];
+    _screenRecognizer.delegate = self;
+    _screenRecognizer.edges = UIRectEdgeRight;
+    [self.view addGestureRecognizer:_screenRecognizer];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+
+    BOOL result = NO;
+    if ([otherGestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
+        result = YES;
+    } else if ([otherGestureRecognizer isKindOfClass:[UIScreenEdgePanGestureRecognizer class]]) {
+        result = YES;
+    }
+    
+    return NO;
+}
+
+- (IBAction)tableViewPanned:(id)sender {
+    UIPanGestureRecognizer *panRecognizer = (UIPanGestureRecognizer *)sender;
+    
+    CGPoint velocity = [panRecognizer velocityInView:_spriteTable];
+    
+    if (panRecognizer.state == UIGestureRecognizerStateBegan) {
+        if (velocity.x > 0) {
+            [UIView animateWithDuration:1 animations:^{
+                CGRect frame = _spriteTable.frame;
+                frame.origin.x += 150;
+                _spriteTable.frame = frame;
+            }];
+        }
+    }
+}
+
+- (IBAction)canvasPanned:(id)sender {
+    UIScreenEdgePanGestureRecognizer *panRecognizer = (UIScreenEdgePanGestureRecognizer *)sender;
+    
+    if (panRecognizer.state == UIGestureRecognizerStateBegan) {
+        [UIView animateWithDuration:1 animations:^{
+            CGRect frame = _spriteTable.frame;
+            frame.origin.x -= 150;
+            _spriteTable.frame = frame;
+        }];
+    }
+}
+
+@end
