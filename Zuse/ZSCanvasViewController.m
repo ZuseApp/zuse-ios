8933df6895a773e4b18d47e1d39362f7c420bc9e
Michael Hogenson <mah.netuse@live.com>
2013-10-02 14:31:06 -0600

    Merged view controllers

--- Zuse/ZSCanvasViewController.m
+++ Zuse/ZSCanvasViewController.m
@@ -7,11 +7,20 @@
 //
 
 #import "ZSCanvasViewController.h"
+#import "TCSpriteManager.h"
+#import "TCSprite.h"
+#import "TCSpriteView.h"
+#import "TCSpriteManager.h"
+#import "INInterpreter.h"
 
 @interface ZSCanvasViewController ()
-    @property (weak, nonatomic) IBOutlet UITableView *spriteTable;
-    @property (strong, nonatomic) UIScreenEdgePanGestureRecognizer *screenRecognizer;
-    @property (assign, nonatomic, getter = isTableViewShowing) BOOL tableViewShowing;
+@property (weak, nonatomic) IBOutlet UITableView *spriteTable;
+@property (strong, nonatomic) UIScreenEdgePanGestureRecognizer *screenRecognizer;
+@property (assign, nonatomic, getter = isTableViewShowing) BOOL tableViewShowing;
+@property (nonatomic, strong) TCSpriteManager *spriteManager;
+@property (nonatomic, strong) NSArray *templateSprites;
+@property (nonatomic, strong) NSArray *canvasSprites;
+@property (strong, nonatomic) INInterpreter *interpreter;
 @end
 
 @implementation ZSCanvasViewController
@@ -34,6 +43,59 @@
     [self.view addGestureRecognizer:_screenRecognizer];
     
     _tableViewShowing = YES;
+    
+#pragma Interpreter
+    NSString *jsonPath = [[NSBundle mainBundle] pathForResource:@"TestProject" ofType:@"json"];
+    NSData *jsonData = [NSData dataWithContentsOfFile:jsonPath];
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:nil];
+    // NSLog(@"%@", json);
+    NSDictionary *jsonObject = [json[@"objects"] firstObject];
+    NSDictionary *variables = jsonObject[@"variables"];
+    CGRect frame = CGRectZero;
+    
+    frame.origin.x = [variables[@"x"] floatValue];
+    frame.origin.y = [variables[@"y"] floatValue];
+    frame.size.width = [variables[@"width"] floatValue];
+    frame.size.height = [variables[@"height"] floatValue];
+    
+    UIView *view = [[UIView alloc] initWithFrame:frame];
+    view.backgroundColor = [UIColor blackColor];
+    
+    [self.view addSubview:view];
+    
+    _interpreter = [[INInterpreter alloc] init];
+    
+    // [_interpreter loadObjects:json[@"objects"]];
+    
+    [_interpreter loadMethod:@{
+                               @"name": @"ask",
+                               @"block":^id(NSArray *args) {
+        return @8;
+    }
+                               }];
+    
+    [_interpreter loadMethod:@{
+                               @"name": @"display",
+                               @"block":^id(NSArray *args) {
+        UIAlertView *alertView = [[UIAlertView alloc] init];
+        [alertView addButtonWithTitle:@"OK"];
+        [alertView setTitle:args[0]];
+        [alertView show];
+        return nil;
+    }
+                               }];
+    
+    [_interpreter loadMethod:@{
+                               @"name": @"random_number",
+                               @"block":^id(NSArray *args) {
+        NSInteger min = [args[0] integerValue];
+        NSInteger max = [args[1] integerValue];
+        NSUInteger rand_num = arc4random_uniform(max) + min;
+        return @(rand_num);
+    }
+                               }];
+    
+    [_interpreter run];
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,7 +104,7 @@
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-
+    
     BOOL result = NO;
     if ([otherGestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
         result = YES;
