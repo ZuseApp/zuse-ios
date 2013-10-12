9ce81153192770388931755f0cd7dea0387c2b50
Michael Hogenson <mah.netuse@live.com>
2013-10-12 15:34:27 -0600

    Added better physics tutorial.

--- Zuse/ZSScene.m
+++ Zuse/ZSScene.m
@@ -8,42 +8,53 @@
 
 #import "ZSScene.h"
 
+@interface ZSScene ()
+
+@property (strong, nonatomic) NSArray *colors;
+
+@end
+
 @implementation ZSScene
 
--(id)initWithSize:(CGSize)size {    
+-(NSArray *) colors {
+    if (!_colors) {
+        _colors = @[[SKColor redColor], [SKColor orangeColor], [SKColor yellowColor], [SKColor greenColor], [SKColor cyanColor], [SKColor blueColor], [SKColor purpleColor]];
+    }
+    
+    return _colors;
+}
+
+-(SKColor *) randomColor {
+    int index = arc4random() % [self colors].count;
+    return [self colors][index];
+}
+
+-(void) addRandomSprite {
+    
+    SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"grayball"];
+    sprite.position = CGPointMake(self.frame.size.width/4 + arc4random() % ((int)self.frame.size.width/2),
+                                  self.frame.size.height/2 + arc4random() % ((int)self.frame.size.height/4));
+    sprite.color = [self randomColor];
+    sprite.colorBlendFactor = 1.0;
+    sprite.xScale = 0.2;
+    sprite.yScale = 0.2;
+    sprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:sprite.size.width / 2];
+    
+    [self addChild:sprite];
+}
+
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-        
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+        self.scaleMode = SKSceneScaleModeAspectFill;
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     }
     return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
+    [self addRandomSprite];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
