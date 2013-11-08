9d6774749b1e6b53ec2248935ecf8cb07064ebd2
Sarah Hong <shong@155-99-161-56.uconnect.utah.edu>
2013-11-08 10:24:58 -0700

    Adding sprite kit scene stuff.

---
+++ Zuse/ZSSarahTestScene.h
@@ -0,0 +1,13 @@
+//
+//  ZSSarahTestScene.h
+//  Zuse
+//
+//  Created by Sarah Hong on 10/25/13.
+//  Copyright (c) 2013 Michael Hogenson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ZSSarahTestScene : SKScene
+
+@end
---
+++ Zuse/ZSSarahTestViewController.h
@@ -0,0 +1,14 @@
+//
+//  ZSSarahTestViewController.h
+//  Zuse
+//
+//  Created by Sarah Hong on 10/25/13.
+//  Copyright (c) 2013 Michael Hogenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ZSSarahTestViewController : UIViewController
+
+@end
