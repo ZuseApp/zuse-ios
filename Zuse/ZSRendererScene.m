d508611ddf9ca820dee63dff1ca394d25fac1c9f
finalfantasyfreak15 <rollingstar.15@gmail.com>
2013-11-22 00:50:37 -0700

    removed monster tutorial code but left the image for the paddles as the monster.png for now

--- Zuse/ZSRendererScene.m
+++ Zuse/ZSRendererScene.m
@@ -13,8 +13,6 @@
 
 @interface ZSRendererScene() <ZSInterpreterDelegate>
 
-@property (nonatomic) SKSpriteNode *player;
-@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
 @property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
 @property (nonatomic) NSMutableDictionary *spriteNodes;
 
@@ -75,11 +73,6 @@
         // setting background color
         self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
         
-        // tutorial code; adding sprite to scene
-//        self.player = [SKSpriteNode spriteNodeWithImageNamed:@"player"];
-//        self.player.position = CGPointMake(self.player.size.width/2, self.frame.size.height/2);
-//        [self addChild:self.player];
-        
     }
     return self;
 }
@@ -98,44 +91,6 @@ didUpdateProperties:(NSDictionary *)properties {
     NSLog(@"%@", properties);
 }
 
-- (void)addMonster {
-    
-    // Create sprite
-    SKSpriteNode * monster = [SKSpriteNode spriteNodeWithImageNamed:@"monster"];
-    
-    // Determine where to spawn the monster along the Y axis
-    int minY = monster.size.height / 2;
-    int maxY = self.frame.size.height - monster.size.height / 2;
-    int rangeY = maxY - minY;
-    int actualY = (arc4random() % rangeY) + minY;
-    
-    // Create the monster slightly off-screen along the right edge,
-    // and along a random position along the Y axis as calculated above
-    monster.position = CGPointMake(self.frame.size.width + monster.size.width/2, actualY);
-    [self addChild:monster];
-    
-    // Determine speed of the monster
-    int minDuration = 2.0;
-    int maxDuration = 4.0;
-    int rangeDuration = maxDuration - minDuration;
-    int actualDuration = (arc4random() % rangeDuration) + minDuration;
-    
-    // Create the actions
-    SKAction * actionMove = [SKAction moveTo:CGPointMake(-monster.size.width/2, actualY) duration:actualDuration];
-    SKAction * actionMoveDone = [SKAction removeFromParent];
-    [monster runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
-    
-}
-
-- (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
-    
-    self.lastSpawnTimeInterval += timeSinceLast;
-    if (self.lastSpawnTimeInterval > 1) {
-        self.lastSpawnTimeInterval = 0;
-        [self addMonster];
-    }
-}
-
 - (void)update:(NSTimeInterval)currentTime {
     // Handle time delta.
     // If we drop below 60fps, we still want everything to move the same distance.
@@ -145,8 +100,6 @@ didUpdateProperties:(NSDictionary *)properties {
         timeSinceLast = 1.0 / 60.0;
         self.lastUpdateTimeInterval = currentTime;
     }
-    
-    [self updateWithTimeSinceLastUpdate:timeSinceLast];
     
 }
 
