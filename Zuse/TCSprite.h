c67d0072ae84b19fda94545f45bcf506bb865b2e
Michael Hogenson <mah.netuse@live.com>
2013-11-30 15:34:51 -0700

    Changed ball

--- Zuse/TCSprite.h
+++ Zuse/TCSprite.h
@@ -3,6 +3,7 @@
 @interface TCSprite : NSObject
 
 @property (strong, nonatomic) NSString *imagePath;
+@property (strong, nonatomic) NSString *imageData;
 @property (assign, nonatomic) CGRect frame;
 @property (assign, nonatomic) NSMutableArray *traits;
 @property(strong, nonatomic) NSMutableArray *code;
