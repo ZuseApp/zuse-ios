95bcee6280d9d62d88fbf36ae27cec07f49b3dde
Parker Wightman <parkerwightman@gmail.com>
2013-12-04 21:12:43 -0700

    Generic popover in place.

--- Zuse/ZSCodeEditorSetStatementTableViewCell.m
+++ Zuse/ZSCodeEditorSetStatementTableViewCell.m
@@ -1,10 +1,15 @@
 #import "ZSCodeEditorSetStatementTableViewCell.h"
+#import "ZSExpressionOptionsTableViewController.h"
 #import "ZSCodeSetStatement.h"
 
-@interface ZSCodeEditorSetStatementTableViewCell()
+#import "ZSPopoverController.h"
+
+@interface ZSCodeEditorSetStatementTableViewCell() <WYPopoverControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *varNameButton;
 @property (weak, nonatomic) IBOutlet UIButton *varValueButton;
+@property (strong, nonatomic) ZSCodeSetStatement *statement;
+@property (strong, nonatomic) ZSPopoverController *popover;
 
 @end
 
@@ -15,10 +20,66 @@
 - (void)setCodeLine:(ZSCodeLine *)codeLine
 {
     super.codeLine = codeLine;
+    _statement = (ZSCodeSetStatement *)self.codeLine.statement;
+    [self updateCellContents];
+}
+
+- (void)updateCellContents {
+    [self.varNameButton setTitle:_statement.variableName forState:UIControlStateNormal];
+    [self.varValueButton setTitle:_statement.variableValueStringValue forState:UIControlStateNormal];
+}
+
+- (IBAction)varNameTapped:(id)sender {
+    ZSExpressionOptionsTableViewController *controller = [[ZSExpressionOptionsTableViewController alloc] initWithStyle:UITableViewStylePlain];
     
-    ZSCodeSetStatement *s = (ZSCodeSetStatement *)self.codeLine.statement;
-    [self.varNameButton setTitle:s.variableName forState:UIControlStateNormal];
-    [self.varValueButton setTitle:s.variableValueStringValue forState:UIControlStateNormal];
+    controller.didSelectValueBlock = ^(id value) {
+        [_popover dismissPopoverAnimated:YES];
+        // TODO: This is wrong right now, need to change once I have variable
+        // listing working correctly.
+        _statement.variableName = [value stringValue];
+        [self updateCellContents];
+    };
+    
+    [self presentExpressionViewController:controller
+                                 fromView:sender];
+}
+
+- (IBAction)varValueTapped:(id)sender {
+    ZSExpressionOptionsTableViewController *controller = [[ZSExpressionOptionsTableViewController alloc] initWithStyle:UITableViewStylePlain];
+    
+    controller.expressionTypeMask = ZSExpressionTypeAny;
+    controller.expressionValueMask = ZSExpressionValueAny;
+    
+    controller.didSelectValueBlock = ^(id value) {
+        [_popover dismissPopoverAnimated:YES];
+        _statement.variableValue = value;
+        [self updateCellContents];
+    };
+    
+    [self presentExpressionViewController:controller
+                                 fromView:sender];
+}
+
+- (void)presentExpressionViewController:(ZSExpressionOptionsTableViewController *)controller
+                               fromView:(UIView *)view{
+    
+    _popover = [[ZSPopoverController alloc] initWithContentViewController:controller];
+    _popover.delegate = self;
+    [_popover presentPopoverFromRect:[view bounds]
+                             inView:view
+           permittedArrowDirections:WYPopoverArrowDirectionUp
+                           animated:YES];
+}
+
+- (BOOL)popoverControllerShouldDismissPopover:(WYPopoverController *)controller
+{
+    return YES;
+}
+
+- (void)popoverControllerDidDismissPopover:(WYPopoverController *)controller
+{
+    _popover.delegate = nil;
+    _popover = nil;
 }
 
 @end
---
+++ Zuse/ZSPopoverController.h
@@ -0,0 +1,13 @@
+//
+//  ZSPopoverController.h
+//  Zuse
+//
+//  Created by Parker Wightman on 12/4/13.
+//  Copyright (c) 2013 Michael Hogenson. All rights reserved.
+//
+
+#import <WYPopoverController/WYPopoverController.h>
+
+@interface ZSPopoverController : WYPopoverController
+
+@end
