9719fe9522d4eb2ebf7d40a961183d352eed1189
finalfantasyfreak15 <rollingstar.15@gmail.com>
2014-01-18 12:58:27 -0700

    Joint physics partially in place, needs more tweaking going forward.

--- Zuse/ZSRendererScene.m
+++ Zuse/ZSRendererScene.m
@@ -8,6 +8,8 @@
 
 #import <SpriteKit-Components/SKComponents.h>
 #import "BlocksKit.h"
+#import <PhysicsDebugger/YMCPhysicsDebugger.h>
+#import <PhysicsDebugger/YMCSKNode+PhysicsDebug.h>
 @import GLKit;
 
 #import "ZSRendererScene.h"
@@ -28,6 +30,14 @@ NSString * const kZSSpriteName = @"sprite";
 NSString * const kZSJointName = @"joint";
 CGFloat const kZSSpriteSpeed = 200;
 
+typedef NS_OPTIONS(uint32_t, CNPhysicsCategory)
+{
+	GFPhysicsCategoryWorld  = 1 << 0,  // 0001 = 1
+	GFPhysicsCategoryJoint  = 1 << 1,  // 0010 = 2
+	GFPhysicsCategoryPaddle = 1 << 2,  // 0100 = 4
+    GFPhysicsCategoryBall   = 1 << 3,
+};
+
 @implementation ZSRendererScene
 
 -(id)initWithSize:(CGSize)size projectJSON:(NSDictionary *)projectJSON {
@@ -41,8 +51,12 @@ CGFloat const kZSSpriteSpeed = 200;
         
         _interpreter.delegate = self;
         
+        //init the debugger
+//        [YMCPhysicsDebugger init];
+        
         //Set the physics edges to the frame
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+        self.physicsBody.categoryBitMask = GFPhysicsCategoryWorld;
         
         // TODO: render objects on screen...
         _spriteNodes = [[NSMutableDictionary alloc] init];
@@ -55,65 +69,103 @@ CGFloat const kZSSpriteSpeed = 200;
             
             //set up the sprite size and position on screen
             SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:object[@"image"][@"path"]];
+            sprite.alpha = 0.1;
             
             node.position = CGPointMake([properties[@"x"] floatValue], [properties[@"y"] floatValue]);
             
             sprite.size = CGSizeMake([properties[@"width"] floatValue], [properties[@"height"] floatValue]);
             
+            //add sprite to the node
+            [node addChild:sprite];
             
             //add the node as a physics body for physics debugging
             SKComponentNode *jointNode = [SKComponentNode new];
+            jointNode.position = CGPointMake([properties[@"x"] floatValue], [properties[@"y"] floatValue]);
             jointNode.name = kZSJointName;
-            jointNode.alpha = 0;
-                                          
+            jointNode.alpha =1.0;
+            //make a transparent sprite for the joint with the same dimensions as the node's sprite
+//            SKSpriteNode *jointSprite = [SKSpriteNode new];
+            SKSpriteNode *jointSprite = [SKSpriteNode spriteNodeWithImageNamed:object[@"image"][@"path"]];
+            jointSprite.size = CGSizeMake([properties[@"width"] floatValue], [properties[@"height"] floatValue]);
+            [jointNode addChild:jointSprite];
+            
             if ([object[@"physics_body"] isEqualToString:@"circle"]) {
                 node.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:(sprite.size.width / 2)];
-                jointNode.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:(sprite.size.width/2)];
+                node.physicsBody.categoryBitMask = GFPhysicsCategoryBall;
+                jointNode.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:(sprite.size.width / 2)];
+                node.physicsBody.collisionBitMask = GFPhysicsCategoryPaddle | GFPhysicsCategoryWorld;
+
             } else {
                 node.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:sprite.size];
-                jointNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:(sprite.size)];
+                jointNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:sprite.size];
+                node.physicsBody.categoryBitMask = GFPhysicsCategoryPaddle;
+                node.physicsBody.collisionBitMask = GFPhysicsCategoryWorld;
+                node.physicsBody.contactTestBitMask = GFPhysicsCategoryJoint;
             }
             
-            SKPhysicsJointFixed *physicsJointFixed = [SKPhysicsJointFixed jointWithBodyA:node.physicsBody bodyB:jointNode.physicsBody anchor:sprite.anchorPoint];
+            jointNode.physicsBody.categoryBitMask = GFPhysicsCategoryJoint;
             
-            ZSSpriteTouchComponent *component = [ZSSpriteTouchComponent new];
+            SKPhysicsJointFixed *physicsJointFixed = [SKPhysicsJointFixed jointWithBodyA:node.physicsBody bodyB:jointNode.physicsBody anchor:node.position];
             
-            component.touchesMoved = ^(UITouch *touch) {
+            ZSSpriteTouchComponent *touchComponent = [ZSSpriteTouchComponent new];
+            
+            touchComponent.touchesMoved = ^(UITouch *touch) {
                 CGPoint point = [touch locationInNode:self];
-                [_interpreter triggerEvent:@"touch_moved"
-                    onObjectWithIdentifier:object[@"id"]
-                                parameters:@{ @"touch_x": @(point.x), @"touch_y": @(point.y) }];
+              
+                NSLog(@"move node position %g, %g", node.position.x, node.position.y);
+                NSLog(@"move joint position %g, %g", jointNode.position.x, jointNode.position.y);
+                
+                    [_interpreter triggerEvent:@"touch_moved"
+                        onObjectWithIdentifier:object[@"id"]
+                                    parameters:@{ @"touch_x": @(point.x), @"touch_y": @(point.y) }];
+                
             };
             
-            component.touchesBegan = ^(UITouch *touch) {
+            touchComponent.touchesBegan = ^(UITouch *touch) {
                 CGPoint point = [touch locationInNode:self];
+                
+                NSLog(@"began node position %g, %g", node.position.x, node.position.y);
+                NSLog(@"began joint position %g, %g", jointNode.position.x, jointNode.position.y);
+                
+                jointNode.position = node.position;
                 [_interpreter triggerEvent:@"touch_began"
                     onObjectWithIdentifier:object[@"id"]
                                 parameters:@{ @"touch_x": @(point.x), @"touch_y": @(point.y) }];
             };
             
-            [jointNode addComponent:component];
+            touchComponent.touchesEnded = ^(UITouch *touch) {
+                CGPoint point = node.position;
+                
+                jointNode.position = node.position;
+                
+//                [_interpreter triggerEvent:@"touch_ended"
+//                    onObjectWithIdentifier:object[@"id"]
+//                                parameters:@{ @"touch_x": @(point.x), @"touch_y": @(point.y) }];
+            };
+            
+            [jointNode addComponent:touchComponent];
             
             //TODO: see if there are issues with fixed joint physics body in case there are sync issues on movement.
-            node.physicsBody.dynamic = NO;
+            node.physicsBody.dynamic = YES;
             node.physicsBody.mass = 0.02;
             node.physicsBody.affectedByGravity = NO;
             
             jointNode.physicsBody.dynamic = NO;
+//            jointNode.physicsBody.dynamic = YES;
             jointNode.physicsBody.mass = 0.02;
             jointNode.physicsBody.affectedByGravity = NO;
-            
-            //add sprite to the node
-            [node addChild:sprite];
-            
             //add both nodes to scene
             [self addChild:node];
             [self addChild:jointNode];
             [self.physicsWorld addJoint:physicsJointFixed];
-
+            
+            
+            //TODO: call debug render method; doesn't seem to render red boxes around physics bodies
+//            [self drawPhysicsBodies];
             
             // ...
             [_spriteNodes setObject:jointNode forKey:object[@"id"]];
+            
         }];
         
         // getting size of screen
@@ -122,11 +174,10 @@ CGFloat const kZSSpriteSpeed = 200;
         // setting background color
         self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
         
-       
-        
     }
     return self;
 }
+
 
 - (void)moveSpriteWithIdentifier:(NSString *)identifier
                        direction:(CGFloat)direction
@@ -149,7 +200,7 @@ CGFloat const kZSSpriteSpeed = 200;
                                GLKVector2 direction = GLKVector2Normalize(velocity);
                                GLKVector2 newVelocity = GLKVector2MultiplyScalar(direction, touchNode.speed);
                                node.physicsBody.velocity = CGVectorMake(newVelocity.x, newVelocity.y);
-//                               node.physicsBody.angularVelocity = 0.0;
+                               node.physicsBody.angularVelocity = 0.0;
                            }];
     
 }
