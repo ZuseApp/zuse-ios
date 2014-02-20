e10ce4a89c948af32feeb46d9f5c966f377e1523
Parker Wightman <parkerwightman@gmail.com>
2014-02-20 15:02:13 -0700

    Beginnings of normalizing invalid input

---
+++ Zuse/ZSPatternMatch.h
@@ -0,0 +1,15 @@
+//
+//  ZSPatternMatch.h
+//  Zuse
+//
+//  Created by Parker Wightman on 2/18/14.
+//  Copyright (c) 2014 Michael Hogenson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZSPatternMatch : NSObject
+
++ (NSArray *) normalizedValuesForCodeSuite:(NSArray *)code;
+
+@end
