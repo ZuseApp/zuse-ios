b1457e0849a873d2967042a6e3d0c7eb9672d520
Parker Wightman <parkerwightman@gmail.com>
2014-02-27 14:30:51 -0700

    Add property scoping.

--- Zuse/ZS_StatementView.h
+++ Zuse/ZS_StatementView.h
@@ -1,11 +1,15 @@
 #import <UIKit/UIKit.h>
 
+@class ZSCodePropertyScope;
+
 @interface ZS_StatementView : UIView
 
 @property (strong, nonatomic) NSMutableDictionary* json;
 @property (strong, nonatomic) NSMutableArray* jsonCode;
 @property (nonatomic, getter = isTopLevelStatement) BOOL topLevelStatement;
 @property (nonatomic, getter = isHighlighted) BOOL highlighted;
+@property (strong, nonatomic) ZSCodePropertyScope *propertyScope;
+@property (strong, nonatomic) NSSet *(^propertiesInScope)();
 
 - (instancetype) initWithJson: (NSMutableDictionary*) json;
 
@@ -14,4 +18,5 @@
 - (void) addArgumentLabelWithText: (NSString*) text touchBlock: (void(^)(UILabel*)) touchBlock;
 - (void) addSubStatementView: (ZS_StatementView*) subStatementView;
 - (void) addNewStatementLabelWithTouchBlock: (void(^)(UILabel*)) touchBlock;
+
 @end
