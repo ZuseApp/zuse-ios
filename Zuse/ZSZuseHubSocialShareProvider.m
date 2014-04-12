3f1286e24c424a14c2a6aa4c15dfaf99f38f873f
finalfantasyfreak15 <rollingstar.15@gmail.com>
2014-04-12 17:20:35 -0600

    working on sharing from canvas

--- Zuse/ZSZuseHubSocialShareProvider.m
+++ Zuse/ZSZuseHubSocialShareProvider.m
@@ -10,4 +10,29 @@
 
 @implementation ZSZuseHubSocialShareProvider
 
+- (id)activityViewController:(UIActivityViewController *)activityViewController itemForActivityType:(NSString *)activityType {
+    // Log out the activity type that we are sharing with
+    NSLog(@"%@", activityType);
+    
+    // Create the default sharing string
+    NSString *shareString = @"Check out the awesome game I made with Zuse!";
+    
+    // customize the sharing string for facebook, twitter, weibo, and google+
+    if ([activityType isEqualToString:UIActivityTypePostToFacebook]) {
+        shareString = [NSString stringWithFormat:@"Attention Facebook: %@", shareString];
+    } else if ([activityType isEqualToString:UIActivityTypePostToTwitter]) {
+        shareString = [NSString stringWithFormat:@"Attention Twitter: %@", shareString];
+    } else if ([activityType isEqualToString:UIActivityTypePostToWeibo]) {
+        shareString = [NSString stringWithFormat:@"Attention Weibo: %@", shareString];
+    } else if ([activityType isEqualToString:@"com.captech.googlePlusSharing"]) {
+        shareString = [NSString stringWithFormat:@"Attention Google+: %@", shareString];
+    }
+    
+    return shareString;
+}
+
+- (id)activityViewControllerPlaceholderItem:(UIActivityViewController *)activityViewController {
+    return @"";
+}
+
 @end
